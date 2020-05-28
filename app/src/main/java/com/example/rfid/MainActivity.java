a90b7bca1cce0d1a0e439bce2f62f0de90f4bf62
Akshay <akshay4262@gmail.com>
2020-05-28 14:39:18 +0530

    Login Screen and Second Activity

---
+++ app/src/main/java/com/example/rfid/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.rfid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText Password;
+    private Button login;
+    private TextView info;
+    private int counter = 5;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Username = (EditText)findViewById(R.id.one);
+        Password = (EditText)findViewById(R.id.pass);
+        login = (Button)findViewById(R.id.log);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checker(Username.getText().toString(), Password.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void checker(String un, String up){
+        if((un.equals("rfid")) && (up.equals("ford"))){
+            Intent intent = new Intent(MainActivity.this, Screentwo.class);
+            startActivity(intent);
+        }else{
+            counter--;
+            info.setText("Number of attempts remaining :" + String.valueOf(counter));
+            if(counter == 0){
+                login.setEnabled(false);
+            }
+        }
+    }
+}
